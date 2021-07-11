36eb8c1ece9baac8e1655ea1d235417cefd00221
Reemafd <87274300+Reemafd@users.noreply.github.com>
2021-07-11 18:58:45 +0300

    Create BK.py

---
+++ BK.py
@@ -0,0 +1,18 @@
+# sudoku
+def solve(bo):
+    find = find_empty(bo)
+    if not find:
+        return True
+    else:
+        row, col = find
+
+    for i in range(1,10):
+        if valid(bo, i, (row, col)):
+            bo[row][col] = i
+
+            if solve(bo):
+                return True
+
+            bo[row][col] = 0
+
+    return False
